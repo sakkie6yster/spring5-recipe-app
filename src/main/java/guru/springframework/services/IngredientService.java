057d7959bbb2a221ce3bb562cde5ac085b02fdf9
izaak <izaak@saicom.io>
2024-05-18 12:41:46 +0200

    Added update Ingredient

--- src/main/java/guru/springframework/services/IngredientService.java
+++ src/main/java/guru/springframework/services/IngredientService.java
@@ -3,6 +3,7 @@ package guru.springframework.services;
 import guru.springframework.commands.IngredientCommand;
 
 public interface IngredientService {
-
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
 }
